5c0fde472e0fe9e73660dd8c1af9adf2a3c3b6a6
Guilherme Rodrigues <melo.guilhermer@gmail.com>
2017-10-28 23:29:48 -0200

    Add

--- view/js/controles.js
+++ view/js/controles.js
@@ -17,7 +17,11 @@ if(porExtensao.checked){
 	document.getElementById('extExe').disabled = false;
 	document.getElementById('extPNG').disabled = false;
 	document.getElementById('extPDF').disabled = false;
-	document.getElementById('extGIF').disabled = false;		
+	document.getElementById('extGIF').disabled = false;
+	document.getElementById('extMP3').disabled = false;		
+	document.getElementById('extMP4').disabled = false;		
+	document.getElementById('extJPG').disabled = false;		
+	document.getElementById('extDOCX').disabled = false;		
 }
 
 
@@ -49,9 +53,13 @@ porExtensao.addEventListener('click', function(){
 		document.getElementById('extGIF').disabled = false;
 	}else{
 		document.getElementById('extExe').disabled = true;
-	document.getElementById('extPNG').disabled = true;
-	document.getElementById('extPDF').disabled = true;
-	document.getElementById('extGIF').disabled = true;
+		document.getElementById('extPNG').disabled = true;
+		document.getElementById('extPDF').disabled = true;
+		document.getElementById('extGIF').disabled = true;
+		document.getElementById('extMP3').disabled = true;
+		document.getElementById('extMP4').disabled = true;
+		document.getElementById('extJPG').disabled = true;
+		document.getElementById('extDOCX').disabled = true;
 	}
 });
 
